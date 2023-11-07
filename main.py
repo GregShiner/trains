c22be618069595b3c8f543c9444a888104005e39
GregShiner <greg@justifinetwork.com>
2023-11-07 09:31:52 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from node import Node, Connection
+
+NODE_DICT = {
+    'A': Node('A', [Connection('B', 'red'), 
+                Connection('B', 'blue'),
+                Connection('C', 'green'),
+                Connection('D', 'red'),
+                Connection('E', 'green'),
+                Connection('F', 'blue'),]),
+    'B': Node('B', [Connection('A', 'red'),
+                Connection('A', 'blue'),
+                Connection('C', 'green'),]),
+    'C': Node('C', [Connection('A', 'green'),
+                Connection('B', 'green'),]),
+    'D': Node('D', [Connection('A', 'red'),]),
+    'E': Node('E', [Connection('A', 'green'),]),
+    'F': Node('F', [Connection('A', 'blue'),]),
+}
+
+LINE_DICT = {
+    'red': ['D', 'A', 'B',],
+    'blue': ['F', 'A', 'B'],
+    'green': ['E', 'A', 'C', 'B'],
+}
+
+def get_node(node_name: str) -> Node:
+    return NODE_DICT[node_name]
---
+++ node.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from typing import List, NamedTuple, TypedDict, Dict, TypeAlias
+
+NodeId = str
+LineId = str
+
+class Connection(NamedTuple):
+    node_id: str # node name of the connected node
+    line_id: str # line name of the connection
+    distance: float = 1 # distance between the nodes
+
+class Node:
+    node_id: str # node name
+    connections: List[Connection] # list of connections
+    def __init__(self, node_id: NodeId, connections: List[Connection] = []):
+        self.node_id = node_id
+        self.connections = connections
+    def get_nodes_on_line(self, line_id: LineId) -> List[NodeId]:
+        return [c.node_id for c in self.connections if c.line_id == line_id]
+    def get_connected_nodes(self) -> List[NodeId]:
+        return [c.node_id for c in self.connections]
+    def get_connected_lines(self) -> List[LineId]:
+        return [c.line_id for c in self.connections]
+    def route_to(self, other: Node) -> List[Connection]:
+        """Returns a list of connections that will take you from this node to the other node"""
+        pass
