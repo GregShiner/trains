76038fe40f544427147d370d2bb0a4b255256333
GregShiner <greg@justifinetwork.com>
2023-11-16 09:21:09 -0500

    Fix main

--- main.py
+++ main.py
@@ -1,37 +1,10 @@
-from typing import Dict
-from node import Node, Connection, NodeId, LineId, Line, Network
+from node import Network
 import ui
 
-NODE_DICT: Dict[NodeId, Node] = {
-    'A': Node('A', [Connection('B', 'red'),
-                    Connection('B', 'blue'),
-                    Connection('C', 'green'),
-                    Connection('D', 'red'),
-                    Connection('E', 'green'),
-                    Connection('F', 'blue'),]),
-
-    'B': Node('B', [Connection('A', 'red'),
-                    Connection('A', 'blue'),
-                    Connection('C', 'green'),]),
-
-    'C': Node('C', [Connection('A', 'green'),
-                    Connection('B', 'green'),]),
-
-    'D': Node('D', [Connection('A', 'red'),]),
-    'E': Node('E', [Connection('A', 'green'),]),
-    'F': Node('F', [Connection('A', 'blue'),]),
-}
-
-LINE_DICT: Dict[LineId, Line] = {
-    'red': ['D', 'A', 'B'],
-    'blue': ['F', 'A', 'B'],
-    'green': ['E', 'A', 'C', 'B'],
-}
-
-network = Network(NODE_DICT)
+network = Network.from_line_file("data.json")
 for node in network.nodes:
     print(node)
-    for connection in NODE_DICT[node].connections:
+    for connection in network.node_dict[node].connections:
         print(f'\t{connection.node_id} on line {connection.line_id}')
 
 for edge in network.edges:
