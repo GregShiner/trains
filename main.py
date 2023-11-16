a5bf80e1cd4c7357463c8905ef6a01ab285c79cc
Greg Salisbury <gregsalisbury25@gmail.com>
2023-11-16 09:08:47 -0500

    Formatting changes

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from typing import Dict
 from node import Node, Connection, NodeId, LineId, Line, Network
-from parser import load_json, line_dict_to_node_dict
 import ui
 
 NODE_DICT: Dict[NodeId, Node] = {
-    'A': Node('A', [Connection('B', 'red'), 
-                Connection('B', 'blue'),
-                Connection('C', 'green'),
-                Connection('D', 'red'),
-                Connection('E', 'green'),
-                Connection('F', 'blue'),]),
+    'A': Node('A', [Connection('B', 'red'),
+                    Connection('B', 'blue'),
+                    Connection('C', 'green'),
+                    Connection('D', 'red'),
+                    Connection('E', 'green'),
+                    Connection('F', 'blue'),]),
+
     'B': Node('B', [Connection('A', 'red'),
-                Connection('A', 'blue'),
-                Connection('C', 'green'),]),
+                    Connection('A', 'blue'),
+                    Connection('C', 'green'),]),
+
     'C': Node('C', [Connection('A', 'green'),
-                Connection('B', 'green'),]),
+                    Connection('B', 'green'),]),
+
     'D': Node('D', [Connection('A', 'red'),]),
     'E': Node('E', [Connection('A', 'green'),]),
     'F': Node('F', [Connection('A', 'blue'),]),
@@ -35,12 +37,7 @@ for node in network.nodes:
 for edge in network.edges:
     print(edge)
 
-#draw(network, with_labels=True)
-#plt.show()
-
 user_stations = ui.prompt_for_stations()
-
-# example is from F to C
 
 user_path = network.route_to(user_stations[0], user_stations[1])
 
