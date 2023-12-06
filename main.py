18ee052fa296d8362fc6ad4483760c20622a96db
GregShiner <greg@justifinetwork.com>
2023-12-05 21:12:30 -0500

    Implement fuzzy search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from node import Network
 import ui
-
+from iterfzf import iterfzf
 
 network = Network.from_line_file("manhattan.json")
 """
@@ -12,9 +12,17 @@ for node in network.nodes:
 for edge in network.edges:
     print(edge)
 """
+nodes = network.node_dict.keys()
+start_node = iterfzf(nodes, multi=False, prompt='Start station: ')
+end_node = iterfzf(nodes, multi=False, prompt='End station: ')
+print(f'Start station: {start_node}')
+print(f'End station: {end_node}')
+
 #user_stations = ui.prompt_for_stations()
-user_stations = ('215th Street (1)', '57th Street (M)')
+#user_stations = ('215th Street (1)', '57th Street (M)')
 
-user_path = network.route_to(user_stations[0], user_stations[1])
+user_path = network.route_to(start_node, end_node) # type: ignore
+#user_path = network.route_to(*user_stations)
 
-ui.display_path(user_stations[0], user_path)
+ui.display_path(start_node, user_path) # type: ignore
+#ui.display_path(user_stations[0], user_path)
